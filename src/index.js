e4c01ffc9efa16efa2de381432f1fe558da2505d
Volodymyr <lotoshead@gmail.com>
2021-10-17 21:11:12 +0300

    +

--- src/index.js
+++
@@ -1,2 +0,0 @@
-import './js/card_list';
-import './js/change_theme';
--- src/js/card_list.js
+++
@@ -1,9 +0,0 @@
-import cardList from '../templates/cards.hbs';
-import menu from '../menu.json';
-
-const menuMarkup = cardList(menu);
-const refs = {
-  cards: document.querySelector('#menu'),
-};
-
-refs.cards.innerHTML = menuMarkup;
